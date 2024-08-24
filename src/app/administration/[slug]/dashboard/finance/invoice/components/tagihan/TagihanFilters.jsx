2222ed54f7fcf07e91c4d919280d4ce0debc0751
LeonardTarigan <leontrg32@gmail.com>
2024-08-24 19:08:41 +0700

    feat: tagihan mobile filter

--- src/app/administration/[slug]/dashboard/finance/invoice/components/tagihan/TagihanFilters.jsx
+++ src/app/administration/[slug]/dashboard/finance/invoice/components/tagihan/TagihanFilters.jsx
@@ -1,11 +1,28 @@
+"use client";
+
 import { SortIcon } from "@/assets/SVGs";
-import { KeyboardArrowDown, Tune } from "@mui/icons-material";
-import { Button, Divider, IconButton, Stack } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import {
+  Button,
+  Checkbox,
+  Divider,
+  Drawer,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { useGetTagihan } from "../../hooks/useGetTagihan";
-import { DateRangeSelect } from "../filters/DateRangeSelect";
+import {
+  DateRangeSelect,
+  TANGGAL_FIELD_NAME,
+} from "../filters/DateRangeSelect";
 import FilterReset from "../filters/FilterReset";
-import { KategoriSelect } from "../filters/KategoriSelect";
-import { StatusSelect } from "../filters/StatusSelect";
+import { KATEGORI_FIELD_NAME, KategoriSelect } from "../filters/KategoriSelect";
+import { STATUS_FIELD_NAME, StatusSelect } from "../filters/StatusSelect";
+import ResetIcon from "../icons/ResetIcon";
 
 const statusFilters = [
   {
@@ -20,6 +37,26 @@ const statusFilters = [
 
 /** @description Component for handling tagihan tab's filters */
 export const TagihanFilters = () => {
+  const theme = useTheme();
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  const toggleDrawer = (newOpen) => () => setOpenDrawer(newOpen);
+
+  const handleResetFilters = () => {
+    router.push(`${pathName}?tab=${tab}`);
+  };
+
+  const tanggal = searchParams.get(TANGGAL_FIELD_NAME);
+  const kategori = searchParams.get(KATEGORI_FIELD_NAME);
+  const status = searchParams.get(STATUS_FIELD_NAME);
+  const tab = searchParams.get("tab") ?? 0;
+
+  const showMobileReset =
+    Boolean(tanggal) || Boolean(kategori) || Boolean(status);
+
   const { data: allBillsData } = useGetTagihan();
 
   const uniqueNames = [
@@ -32,35 +69,137 @@ export const TagihanFilters = () => {
   }));
 
   return (
-    <Stack flexDirection="row" alignItems="center" gap={1}>
-      <Button
-        fullWidth
-        variant="outlined"
-        className="flex justify-between lg:hidden"
-        startIcon={<SortIcon />}
-        endIcon={<KeyboardArrowDown />}
-      >
-        <span className="mr-auto">Urutkan</span>
-      </Button>
-      <Divider
-        orientation="vertical"
-        sx={{ height: 36.5, display: { xs: "block", lg: "none" } }}
-      />
-      <IconButton
-        sx={{ borderRadius: 2, display: { xs: "block", lg: "none" } }}
-      >
-        <Tune color="primary" />
-      </IconButton>
+    <>
+      <Stack flexDirection="row" alignItems="center" gap={1}>
+        <IconButton
+          sx={{
+            borderRadius: 2,
+            display: { xs: "block", lg: "none" },
+          }}
+          onClick={toggleDrawer(true)}
+        >
+          <SortIcon sx={{ color: theme.palette.primary.main }} />
+        </IconButton>
+        <Drawer
+          anchor="bottom"
+          sx={{ borderRadius: "20px 20px 0 0 !important" }}
+          open={openDrawer}
+          onClose={toggleDrawer(false)}
+        >
+          <DrawerContent toggleDrawer={toggleDrawer} />
+        </Drawer>
+        <Divider
+          orientation="vertical"
+          sx={{ height: 36.5, display: { xs: "block", lg: "none" } }}
+        />
+        <Stack
+          flexDirection={"row"}
+          alignItems={"center"}
+          gap={1}
+          overflow={"auto"}
+          sx={{ display: { xs: "flex", lg: "none" } }}
+        >
+          <div className="shrink-0 flex gap-2">
+            <DateRangeSelect />
+          </div>
+          <KategoriSelect data={nameFilters} />
+          <StatusSelect data={statusFilters} />
+        </Stack>
+
+        <Stack
+          display={{ xs: "none", lg: "flex" }}
+          flexDirection="row"
+          alignItems="center"
+          gap={1}
+        >
+          <DateRangeSelect />
+          <KategoriSelect data={nameFilters} />
+          <StatusSelect data={statusFilters} />
+          <FilterReset />
+        </Stack>
+      </Stack>
+      {showMobileReset && (
+        <Button
+          startIcon={<ResetIcon color={theme.palette.primary.main} />}
+          onClick={handleResetFilters}
+        >
+          Reset
+        </Button>
+      )}
+    </>
+  );
+};
+
+const filters = [
+  {
+    label: "ID Tagihan",
+    value: "id",
+  },
+  {
+    label: "Nama Tagihan",
+    value: "name",
+  },
+  {
+    label: "Total harga",
+    value: "amount",
+  },
+  {
+    label: "Jumlah Pembayaran",
+    value: "total_paid",
+  },
+  {
+    label: "Tenggat Waktu",
+    value: "deadline",
+  },
+  {
+    label: "Status",
+    value: "status",
+  },
+];
+
+const DrawerContent = ({ toggleDrawer }) => {
+  return (
+    <Stack padding={"16px"}>
       <Stack
-        display={{ xs: "none", lg: "flex" }}
-        flexDirection="row"
-        alignItems="center"
-        gap={1}
+        flexDirection={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
       >
-        <DateRangeSelect />
-        <KategoriSelect data={nameFilters} />
-        <StatusSelect data={statusFilters} />
-        <FilterReset />
+        <Typography fontWeight={600} fontSize={"18px"}>
+          Urutkan
+        </Typography>
+        <IconButton
+          sx={{ borderRadius: 2, display: { xs: "block", lg: "none" } }}
+          onClick={toggleDrawer(false)}
+        >
+          <Close color="action" />
+        </IconButton>
+      </Stack>
+      <Divider sx={{ marginY: "16px" }} />
+      <form>
+        <Stack gap={"16px"}>
+          {filters.map(({ label, value }) => (
+            <Stack gap={"16px"}>
+              <Stack
+                flexDirection={"row"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+              >
+                <Typography>{label}</Typography>
+                <Checkbox />
+              </Stack>
+              <Divider />
+            </Stack>
+          ))}
+        </Stack>
+      </form>
+      <Stack flexDirection={"row"} gap={"12px"} marginTop={"32px"}>
+        <Button onClick={toggleDrawer(false)} variant="outlined" fullWidth>
+          Batal
+        </Button>
+        <Button variant="contained" fullWidth>
+          Simpan
+        </Button>
       </Stack>
     </Stack>
   );
