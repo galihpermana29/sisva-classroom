40ae4a3bc81b8f41b7241d70fc545f434e19b091
Hilaladiii <abdulhilal242@gmail.com>
2024-09-26 18:09:59 +0700

    chore : update skeleton card color and size

--- src/app/classroom/shared/presentation/Skeletons/CardClassSkeleton.jsx
+++ src/app/classroom/shared/presentation/Skeletons/CardClassSkeleton.jsx
@@ -3,7 +3,7 @@ import { BoxTop } from "@/app/classroom/shared/presentation/Box/Box";
 
 const CardClassSkeleton = () => {
   return (
-    <div className="flex-none bg-secondary50 p-3 rounded-xl relative overflow-hidden w-full lg:w-fit animate-pulse">
+    <div className="flex-none bg-gray-200 p-3 rounded-xl relative overflow-hidden w-full lg:w-fit animate-pulse">
       <BoxTop className="size-[104px] -right-5 -top-12" rotate={-67.677} />
       <Flex gap={16}>
         <div className="w-12 h-12 bg-gray-300 rounded-full"></div>
@@ -23,7 +23,7 @@ const CardClassSkeleton = () => {
       <div className="w-24 h-3 bg-gray-300 rounded mb-2"></div>
 
       <div className="w-[280px] lg:w-[265px] p-3 rounded-xl border shadow-card bg-white relative animate-pulse">
-        <div className="w-1 h-10 absolute rounded-r-md left-0 top-1/2 -translate-y-1/2 bg-secondary50"></div>
+        <div className="w-1 h-10 absolute rounded-r-md left-0 top-1/2 -translate-y-1/2 bg-gray-400"></div>
         <Flex vertical gap={2}>
           <div className="w-40 h-4 bg-gray-300 rounded"></div>
           <div className="w-20 h-3 bg-gray-300 rounded"></div>
--- src/app/classroom/shared/presentation/Skeletons/CardScheduleSkeleton.jsx
+++ src/app/classroom/shared/presentation/Skeletons/CardScheduleSkeleton.jsx
@@ -6,19 +6,16 @@ const CardScheduleSkeleton = ({ isEven }) => {
       <Flex align="center" gap={8} className="animate-pulse">
         <div className="h-4 bg-gray-300 rounded w-10"></div>
         <div
-          className={`w-full p-3 rounded-xl border border-secondary50 shadow-card mr-3 relative overflow-hidden ${
+          className={`w-full p-3 rounded-xl border  shadow-card mr-3 relative overflow-hidden ${
             isEven ? "bg-gray-200" : "bg-gray-300"
           }`}
-          style={{
-            border: "1px solid var(--Secondary-50, #F96756)",
-          }}
         >
           <div
             className={`w-1 h-10 absolute rounded-r-md left-0 top-1/2 -translate-y-1/2 ${
-              isEven ? "bg-secondary50" : "bg-secondary70"
+              isEven ? "bg-gray-400" : "bg-gray-500"
             }`}
           ></div>
-          <Flex vertical gap={2}>
+          <Flex vertical gap={3}>
             <div className="h-4 bg-gray-300 rounded w-3/4"></div>
             <div className="h-3 bg-gray-300 rounded w-1/2"></div>
           </Flex>
