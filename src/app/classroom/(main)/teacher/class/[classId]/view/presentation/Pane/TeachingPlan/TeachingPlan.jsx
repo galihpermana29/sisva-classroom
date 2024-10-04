2b4966df193182e4024cef1d023835dfc9edf439
rmvs <nofalrmz@gmail.com>
2024-10-04 20:12:27 +0700

    refactor: implement skeleton for loading state

--- src/app/classroom/(main)/teacher/class/[classId]/view/presentation/Pane/TeachingPlan/TeachingPlan.jsx
+++ src/app/classroom/(main)/teacher/class/[classId]/view/presentation/Pane/TeachingPlan/TeachingPlan.jsx
@@ -8,47 +8,41 @@ import TeachingPlanList from "./TeachingPlanList";
 
 import { useTeachingPlan } from "../../../../usecase/hooks/use-teaching-plan";
 import TeachingPlanTitle from "./TeachingPlanTitle";
+import TeachingPlanListSkeleton from "./TeachingPlanListSkeleton";
 
 const TeachingPlanPane = () => {
   const { teachingPlans, isLoading } = useTeachingPlan();
 
   return (
-    <div className="font-normal max-w-6xl mx-auto text-black font-kumbh">
-      <div className="hidden md:flex items-center justify-between">
+    <div className="max-w-6xl mx-auto font-normal text-black font-kumbh">
+      <div className="items-center justify-between hidden md:flex">
         <h1 className="text-2xl font-bold">Rencana Pembelajaran</h1>
         <SisvaButton className="font-bold" icon={<Plus className="size-5" />}>
           <span className="text-sm font-bold">Tambah Rencana</span>
         </SisvaButton>
       </div>
 
-      <div className="md:mt-6">
+      <div className="grid gap-4 divide-y md:mt-6">
         {isLoading ? (
-          <div>loading...</div>
-        ) : teachingPlans.length == 0 ? (
-          <>
-            <TeachingPlanTitle title="Add Topics" />
-            <EmptyState />
-          </>
+          <TeachingPlanListSkeleton />
         ) : (
-          <div className="grid gap-4 divide-y">
-            {teachingPlans.map((plan, idx) => (
-              <TeachingPlanList
-                key={"teaching_plan" + idx}
-                id={plan.id}
-                title={plan.title}
-                tasks={plan.tasks}
-                description={plan.description}
-                teaching_materials={plan.teaching_materials}
-                teaching_goal={plan.teaching_goal}
-                teaching_activity={plan.teaching_activity}
-                teaching_scoring={plan.teaching_scoring}
-              />
-            ))}
-
-            <TeachingPlanTitle title="Add Topics" />
-            <EmptyState className="!mt-1" />
-          </div>
+          teachingPlans.map((plan, idx) => (
+            <TeachingPlanList
+              key={"teaching_plan" + idx}
+              id={plan.id}
+              title={plan.title}
+              tasks={plan.tasks}
+              description={plan.description}
+              teaching_materials={plan.teaching_materials}
+              teaching_goal={plan.teaching_goal}
+              teaching_activity={plan.teaching_activity}
+              teaching_scoring={plan.teaching_scoring}
+            />
+          ))
         )}
+
+        <TeachingPlanTitle title="Add Topics" />
+        <EmptyState className="!mt-1" />
       </div>
     </div>
   );
