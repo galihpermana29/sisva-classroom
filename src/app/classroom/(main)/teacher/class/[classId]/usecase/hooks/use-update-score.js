b875e11a003a96a07ac74dd62c8819c942493eab
Hilaladiii <abdulhilal242@gmail.com>
2024-10-07 23:40:29 +0700

    chore: delete console.log

--- src/app/classroom/(main)/teacher/class/[classId]/usecase/hooks/use-update-score.js
+++ src/app/classroom/(main)/teacher/class/[classId]/usecase/hooks/use-update-score.js
@@ -7,13 +7,8 @@ export function useUpdateScore() {
 
   const updateScoreStudent = async (id, data) => {
     setLoading(true);
-    console.log("id");
-    console.log(id);
-    console.log("data");
-    console.log(data);
 
     const res = await updateScore(id, data);
-    console.log(res.message);
 
     if (res.success) {
       setLoading(false);
