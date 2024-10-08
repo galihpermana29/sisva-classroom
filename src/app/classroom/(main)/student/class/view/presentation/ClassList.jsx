0ddbcc619f2f964ae9c76ccfe9c3eddd1e32ebf9
rmvs <nofalrmz@gmail.com>
2024-10-08 18:23:46 +0700

    fix: centering empy layout

--- src/app/classroom/(main)/student/class/view/presentation/ClassList.jsx
+++ src/app/classroom/(main)/student/class/view/presentation/ClassList.jsx
@@ -5,29 +5,31 @@ import CardClassSkeleton from "@/app/classroom/shared/presentation/Skeletons/Car
 
 const StudentClassList = ({ classes, isLoading }) => {
   return (
-    <div className="grid gap-4 p-6 mt-6 shadow md:grid-cols-2 xl:grid-cols-3 rounded-xl md:border-none md:shadow-card">
-      {isLoading ? (
-        Array.from({ length: 6 }).map((_, index) => (
-          <CardClassSkeleton key={index} />
-        ))
-      ) : classes.length == 0 ? (
+    <div className="grid p-6 mt-6 shadow rounded-xl md:border-none md:shadow-card">
+      {classes.length == 0 ? (
         <EmptyData
           title="Tidak ada kelas"
           subtitle="Tidak ada kelas yang tersedia untuk saat ini"
         />
       ) : (
-        classes.map((classItem, index) => (
-          <ClassCard
-            key={"class_" + index}
-            subject={classItem.subject_name}
-            teacherPhoto={classItem.teacher_photo}
-            teacherName={classItem.teacher_name}
-            group={classItem.student_group_name}
-            taskName={classItem.tasks[0]?.name}
-            timeStamp={classItem.tasks[0]?.deadline}
-            isEmptyTask={classItem.tasks.length === 0}
-          />
-        ))
+        <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3 ">
+          {isLoading
+            ? Array.from({ length: 6 }).map((_, index) => (
+                <CardClassSkeleton key={index} />
+              ))
+            : classes.map((classItem, index) => (
+                <ClassCard
+                  key={"class_" + index}
+                  subject={classItem.subject_name}
+                  teacherPhoto={classItem.teacher_photo}
+                  teacherName={classItem.teacher_name}
+                  group={classItem.student_group_name}
+                  taskName={classItem.tasks[0]?.name}
+                  timeStamp={classItem.tasks[0]?.deadline}
+                  isEmptyTask={classItem.tasks.length === 0}
+                />
+              ))}
+        </div>
       )}
     </div>
   );
