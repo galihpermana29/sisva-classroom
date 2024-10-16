ab0513762cd3ee46eb47c5cd779766ff8b6d6b48
Hilaladiii <abdulhilal242@gmail.com>
2024-10-16 23:29:30 +0700

    refactor : fix foldering

---
+++ src/app/classroom/(main)/student/class/[id]/task/[task_id]/container/Skeleton/SkeletonDetailTask.jsx
@@ -0,0 +1,31 @@
+const SkeletonDetailTask = () => {
+  return (
+    <div className="bg-white p-4 rounded-xl shadow-card">
+      <div className="h-8 bg-gray-300 rounded w-1/5 animate-pulse"></div>
+
+      <div className="mt-5 flex justify-between">
+        <div className="h-6 bg-gray-300 rounded w-1/4 animate-pulse"></div>
+        <div className="h-6 bg-gray-300 rounded w-1/4 animate-pulse"></div>
+        <div className="h-6 bg-gray-300 rounded w-1/4 animate-pulse"></div>
+      </div>
+
+      <br />
+
+      <div className="w-full">
+        <div className="h-4 bg-gray-300 rounded w-32 animate-pulse"></div>
+        <div className="h-4 bg-gray-300 rounded w-full animate-pulse mt-2"></div>
+      </div>
+
+      <div
+        className="w-full bg-[#F9F9F9] rounded-lg p-4 mt-5"
+        style={{
+          border: "solid 1px #d0d5dd",
+        }}
+      >
+        <div className="h-8 bg-gray-300 rounded w-full animate-pulse"></div>
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonDetailTask;
---
+++ src/app/classroom/(main)/student/class/[id]/task/[task_id]/usecase/use-task-detail.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import {
+  getAllClasses,
+  getStudentGroups,
+  getTaskById,
+} from "../repository/task-submission-repository";
+import { getClientSession } from "@/app/classroom/shared/usecase/session/get-client-session";
+
+export function useTask() {
+  const [task, setTask] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  const params = useParams();
+  const { task_id } = params;
+  const { id: student_id } = getClientSession();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {
+        data: studentGroups,
+        success: successStudentGroups,
+        message: messageStudentGroups,
+      } = await getStudentGroups();
+
+      if (!successStudentGroups || !Array.isArray(studentGroups)) {
+        setLoading(false);
+        setError(messageStudentGroups);
+      }
+
+      const foundStudentGroup = studentGroups.find(
+        (student) => student.student_id == student_id
+      );
+
+      const {
+        data: classes,
+        success: successClasses,
+        message: messageClasses,
+      } = await getAllClasses();
+
+      if (!successClasses || !Array.isArray(classes)) {
+        setLoading(false);
+        setError(messageClasses);
+      }
+
+      const classStudent = classes.find(
+        (cls) => cls.student_group_id == foundStudentGroup.student_group_id
+      );
+
+      const {
+        data: taskData,
+        success: successTaskId,
+        message: messageTaskId,
+      } = await getTaskById(task_id);
+
+      if (!successTaskId) {
+        setLoading(false);
+        setError(messageTaskId);
+        router.back();
+      }
+
+      setTask({
+        task_name: taskData.name,
+        task_deadline: taskData.deadline,
+        task_allow_submission: taskData.allow_submission,
+        teacher_name: classStudent.teacher_name,
+        task_description: taskData.description,
+        task_file: taskData.attachment_file_uri,
+      });
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  return {
+    task,
+    error,
+    loading,
+  };
+}
