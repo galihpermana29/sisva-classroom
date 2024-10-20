53e8e0245fa76220a5ddf591320f6c78b67cef9d
rmvs <nofalrmz@gmail.com>
2024-10-20 17:24:03 +0700

    fix: add source header, format date, filter by overdue, add routing

--- src/app/classroom/(main)/teacher/usecase/convertDateTime12To24.js
+++ src/app/classroom/(main)/teacher/usecase/convertDateTime12To24.js
@@ -4,15 +4,8 @@
  * @returns {string}
  */
 export function convertDateTime12To24(dateTime) {
-  const [datePart, timePart] = dateTime.split(" +")[0].split(" ");
-
-  const fullDateString = `${datePart} ${timePart}`;
-
-  const date = new Date(fullDateString);
-
-  const formattedDate = date.toLocaleDateString("en-GB");
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  return `${formattedDate} ${hours}:${minutes}`;
+  const [day, month, yearAndTime] = dateTime.split("/");
+  const [year, time] = yearAndTime.split(" ");
+  const formatted = `${month}/${day}/${year} ${time}`;
+  return formatted;
 }
