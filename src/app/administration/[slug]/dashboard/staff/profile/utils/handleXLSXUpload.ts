be1497e96e7ec1504016e09b5b1f60c39c02c687
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-10-22 18:50:41 +0700

    Refactor try catch

--- src/app/administration/[slug]/dashboard/staff/profile/utils/handleXLSXUpload.ts
+++ src/app/administration/[slug]/dashboard/staff/profile/utils/handleXLSXUpload.ts
@@ -29,72 +29,71 @@ export default function handleXLSXUpload(file: File) {
   const reader = new FileReader();
   reader.onload = async (e) => {
     const file = e.target.result;
-    const template = XLSX.read(file);
-    const sheet = template.Sheets[template.SheetNames[0]];
-    const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-    const rawDataWithoutHeader = rawData.slice(3, MAX_ROW + 3);
-    const dataObject = rawDataWithoutHeader
-      .map((row) => {
-        function getPermissions() {
-          const permissions: Permission[] = [];
-          if (row[3]) permissions.push('manage_school');
-          if (row[4]) permissions.push('manage_staff');
-          if (row[5]) permissions.push('manage_academic');
-          if (row[6]) permissions.push('manage_student');
-          if (row[7]) permissions.push('report');
-          if (row[8]) permissions.push('manage_information');
-          if (row[9]) permissions.push('manage_finance');
-          return permissions;
-        }
-
-        function getRole(): Role {
-          switch (row[2]) {
-            case 'Staf':
-              return 'staff';
-            case 'Guru':
-              return 'teacher';
-            case 'Manajemen':
-              return 'management';
+    try {
+      const template = XLSX.read(file);
+      const sheet = template.Sheets[template.SheetNames[0]];
+      const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+      const rawDataWithoutHeader = rawData.slice(3, MAX_ROW + 3);
+      const dataObject = rawDataWithoutHeader
+        .map((row) => {
+          function getPermissions() {
+            const permissions: Permission[] = [];
+            if (row[3]) permissions.push('manage_school');
+            if (row[4]) permissions.push('manage_staff');
+            if (row[5]) permissions.push('manage_academic');
+            if (row[6]) permissions.push('manage_student');
+            if (row[7]) permissions.push('report');
+            if (row[8]) permissions.push('manage_information');
+            if (row[9]) permissions.push('manage_finance');
+            return permissions;
           }
-        }
 
-        return {
-          name: row[0],
-          username: row[1],
-          type: getRole(),
-          permissions: getPermissions(),
-          password: row[10],
-        };
-      })
-      .filter((data) => data.name);
+          function getRole(): Role {
+            switch (row[2]) {
+              case 'Staf':
+                return 'staff';
+              case 'Guru':
+                return 'teacher';
+              case 'Manajemen':
+                return 'management';
+            }
+          }
 
-    const promises = dataObject.map((data) => {
-      const payload = {
-        user: {
-          name: data.name,
-          type: data.type,
-          detail: {
-            json_text: JSON.stringify({
-              username: data.username,
-            }),
+          return {
+            name: row[0],
+            username: row[1],
+            type: getRole(),
+            permissions: getPermissions(),
+            password: row[10],
+          };
+        })
+        .filter((data) => data.name);
+
+      const promises = dataObject.map((data) => {
+        const payload = {
+          user: {
+            name: data.name,
+            type: data.type,
+            detail: {
+              json_text: JSON.stringify({
+                username: data.username,
+              }),
+            },
+            profile_image_uri: '',
+            roles: [data.type],
+            permissions: data.permissions,
           },
-          profile_image_uri: '',
-          roles: [data.type],
-          permissions: data.permissions,
-        },
-        password: data.password,
-      };
-      console.log(payload);
-      return UsersAPI.createUser(payload);
-    });
+          password: data.password,
+        };
+        console.log(payload);
+        return UsersAPI.createUser(payload);
+      });
 
-    await Promise.all(promises);
+      await Promise.all(promises);
+    } catch (error) {
+      console.log(error);
+      window.alert('Import Gagal');
+    }
   };
-
-  try {
-    reader.readAsArrayBuffer(file);
-    return true;
-  } catch (error) {
-    return false;
-  }
+  reader.readAsArrayBuffer(file);
 }
