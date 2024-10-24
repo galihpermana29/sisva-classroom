5c248d50211c18271d1a5edb9dc74c8b07646a9f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-10-24 11:24:22 +0700

    Add column comment

--- src/app/administration/[slug]/dashboard/student/profile/utils/handleXLSXUpload.ts
+++ src/app/administration/[slug]/dashboard/student/profile/utils/handleXLSXUpload.ts
@@ -12,6 +12,51 @@ import * as XLSX from 'xlsx';
   0: name
   1: username
   2: password
+  3: email
+  4: phone
+  5: gender
+  6: nationality
+  7: personal_id
+  8: education_id
+  9: religion
+  10: address
+  11: profile_image_uri
+  12: guardian_type
+
+  13: father_name
+  14: father_email
+  15: father_phone
+  16: father_occupation
+  17: father_education
+  18: father_income
+  19: father_birth_year
+  20: father_life_status
+  21: father_religion
+  22: father_address
+
+  23: mother_name
+  24: mother_email
+  25: mother_phone
+  26: mother_occupation
+  27: mother_education
+  28: mother_income
+  29: mother_birth_year
+  30: mother_life_status
+  31: mother_religion
+  32: mother_address
+
+  33: guardian_name
+  34: guardian_gender
+  35: guardian_relationship
+  36: guardian_email
+  37: guardian_phone
+  38: guardian_occupation
+  39: guardian_education
+  40: guardian_income
+  41: guardian_birth_year
+  42: guardian_life_status
+  43: guardian_religion
+  44: guardian_address
 
 */
 
