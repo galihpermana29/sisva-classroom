634fc4a20b5f6ac865f240265b72150088d8f72b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-10-26 12:18:34 +0700

    Remove zombie image

--- src/app/administration/[slug]/auth/layout.js
+++ src/app/administration/[slug]/auth/layout.js
@@ -3,16 +3,9 @@
 import { Box, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
 
-// export const metadata = {
-//   title: 'Login | Sisva',
-//   description: 'Sisva | Solusi Digitalisasi dan Modernisasi Sekolah',
-// };
-
-import LogoSisva from '@/assets/Sisva-LogoType-Black.png';
-import LoginLibrary from '@/assets/Login-Library.png';
-import SchoolLogoBlue from '@/assets/School-Logo-Blue.png';
-import { useParams } from 'next/navigation';
 import CmsAPI from '@/api/cms';
+import LogoSisva from '@/assets/Sisva-LogoType-Black.png';
+import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 export default function RootLayout({ children }) {
@@ -47,7 +40,7 @@ export default function RootLayout({ children }) {
         }}
       >
         <Image
-          alt='Web Image'
+          alt="Web Image"
           src={`https://api-staging.sisva.id/file/v1/files/${profileData?.landing_image_uri}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
           layout={'fill'}
           objectFit={'cover'}
@@ -62,21 +55,15 @@ export default function RootLayout({ children }) {
           alignItems: 'center',
         }}
       >
-        <Stack alignItems='center'>
-          <Image
-            alt='Web Image'
-            src={`https://api-staging.sisva.id/file/v1/files/${profileData?.logo_uri}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
-            width={110}
-            height={110}
-          />
-          <Typography fontWeight='700' mt={1} fontSize={24}>
+        <Stack alignItems="center">
+          <Typography fontWeight="700" mt={1} fontSize={24}>
             {profileData?.name}
           </Typography>
-          <Stack direction='row' alignItems='center' gap='6px'>
-            <Typography variant='body2' color='grey'>
+          <Stack direction="row" alignItems="center" gap="6px">
+            <Typography variant="body2" color="grey">
               Powered by
             </Typography>
-            <Image alt='Web Image' src={LogoSisva} width={56} height={30} />
+            <Image alt="Web Image" src={LogoSisva} width={56} height={30} />
           </Stack>
         </Stack>
         {children}
