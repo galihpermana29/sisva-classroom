8874cf3170461701ad6789e365e06428e539ea68
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-10-26 17:16:09 +0700

    Cached Table

--- src/app/administration/[slug]/dashboard/staff/profile/components/Table.tsx
+++ src/app/administration/[slug]/dashboard/staff/profile/components/Table.tsx
@@ -1,22 +1,22 @@
 import { permissions, types } from '@/globalcomponents/Variable';
 import { BorderColorRounded, DeleteForeverRounded } from '@mui/icons-material';
 import {
-    Avatar,
-    Box,
-    Button,
-    Chip,
-    IconButton,
-    Modal,
-    Paper,
-    Stack,
-    Typography,
-    useMediaQuery,
+  Avatar,
+  Box,
+  Button,
+  Chip,
+  IconButton,
+  Modal,
+  Paper,
+  Stack,
+  Typography,
+  useMediaQuery,
 } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 const columns = [
   {
@@ -35,7 +35,7 @@ const columns = [
         <Box sx={{ width: '100%', mx: 2, py: 0.5 }}>
           <Stack
             component={Paper}
-            variant='outlined'
+            variant="outlined"
             sx={{
               justifyContent: 'flex-start',
               borderRadius: 2,
@@ -54,9 +54,9 @@ const columns = [
                 >
                   {params.value.data.profile_image_uri !== '' ? (
                     <Image
-                      alt='Web Image'
+                      alt="Web Image"
                       fill
-                      sizes='100%'
+                      sizes="100%"
                       style={{ objectFit: 'cover' }}
                       src={`https://api-staging.sisva.id/file/v1/files/${params.value.data.profile_image_uri}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
                     />
@@ -123,9 +123,9 @@ const columns = [
       >
         {params.value[0] !== '' ? (
           <Image
-            alt='Web Image'
+            alt="Web Image"
             fill
-            sizes='100%'
+            sizes="100%"
             style={{ objectFit: 'cover' }}
             src={`https://api-staging.sisva.id/file/v1/files/${params.value[0]}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
           />
@@ -196,7 +196,7 @@ function ChipList({ params }) {
               fontSize: 12,
             }}
             label={tempPermission}
-            color='primary'
+            color="primary"
           />
         );
       })}
@@ -252,7 +252,13 @@ function ActionButton({ params }) {
   );
 }
 
-export default function DataTable({ data, deleteUser = () => ({}) }) {
+export default function DataTable({
+  data,
+  deleteUser = () => {},
+}: {
+  data: any;
+  deleteUser: (userData: any) => void;
+}) {
   const isMobile = useMediaQuery((theme) => theme.breakpoints.down('lg'));
 
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
@@ -282,193 +288,197 @@ export default function DataTable({ data, deleteUser = () => ({}) }) {
     rows.push(tempObject);
   });
 
-  return (
-    <div style={{ height: '100%', width: '100%' }}>
-      <Modal open={openDeleteModal} onClose={() => setOpenDeleteModal(false)}>
-        <Stack
-          component={Paper}
-          elevation={2}
-          sx={{
-            borderRadius: 2,
-            zIndex: 20,
-            margin: 'auto',
-            position: 'fixed',
-            height: 'fit-content',
-            width: '360px',
-            maxWidth: '80%',
-            top: 0,
-            bottom: 0,
-            right: 0,
-            left: 0,
-            p: 2,
-          }}
-        >
-          <Box>
-            <Typography fontWeight={600} fontSize={16}>
-              Hapus Karyawan
-            </Typography>
-          </Box>
-
-          <Typography sx={{ mt: 1, fontSize: 14 }}>
-            Anda akan menghapus karyawan berikut:
-          </Typography>
+  const CachedReturn = useCallback(() => {
+    return (
+      <div style={{ height: '100%', width: '100%' }}>
+        <Modal open={openDeleteModal} onClose={() => setOpenDeleteModal(false)}>
           <Stack
+            component={Paper}
+            elevation={2}
             sx={{
-              backgroundColor: 'base.base20',
-              p: 1,
               borderRadius: 2,
-              flexDirection: 'row',
-              alignItems: 'center',
-              mt: 1,
-              mb: 2,
+              zIndex: 20,
+              margin: 'auto',
+              position: 'fixed',
+              height: 'fit-content',
+              width: '360px',
+              maxWidth: '80%',
+              top: 0,
+              bottom: 0,
+              right: 0,
+              left: 0,
+              p: 2,
             }}
           >
-            <Avatar
+            <Box>
+              <Typography fontWeight={600} fontSize={16}>
+                Hapus Karyawan
+              </Typography>
+            </Box>
+
+            <Typography sx={{ mt: 1, fontSize: 14 }}>
+              Anda akan menghapus karyawan berikut:
+            </Typography>
+            <Stack
               sx={{
-                width: '40px',
-                height: '40px',
-                position: 'relative',
-                mr: 1,
+                backgroundColor: 'base.base20',
+                p: 1,
+                borderRadius: 2,
+                flexDirection: 'row',
+                alignItems: 'center',
+                mt: 1,
+                mb: 2,
               }}
             >
-              {activeRow.profile_image_uri !== '' ? (
-                <Image
-                  alt='Web Image'
-                  fill
-                  sizes='100%'
-                  style={{ objectFit: 'cover' }}
-                  src={`https://api-staging.sisva.id/file/v1/files/${activeRow.profile_image_uri}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
-                />
-              ) : (
-                activeRow.name?.toUpperCase().slice(0, 1)
-              )}
-            </Avatar>
-            <Stack justifyContent={'center'}>
-              <Typography
+              <Avatar
                 sx={{
-                  color: 'black',
-                  fontWeight: 600,
+                  width: '40px',
+                  height: '40px',
+                  position: 'relative',
+                  mr: 1,
                 }}
               >
-                {activeRow.name}
-              </Typography>
-              <Typography sx={{ fontSize: 14, lineHeight: '16px' }}>
-                {activeRow.username}
-              </Typography>
+                {activeRow.profile_image_uri !== '' ? (
+                  <Image
+                    alt="Web Image"
+                    fill
+                    sizes="100%"
+                    style={{ objectFit: 'cover' }}
+                    src={`https://api-staging.sisva.id/file/v1/files/${activeRow.profile_image_uri}?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
+                  />
+                ) : (
+                  activeRow.name?.toUpperCase().slice(0, 1)
+                )}
+              </Avatar>
+              <Stack justifyContent={'center'}>
+                <Typography
+                  sx={{
+                    color: 'black',
+                    fontWeight: 600,
+                  }}
+                >
+                  {activeRow.name}
+                </Typography>
+                <Typography sx={{ fontSize: 14, lineHeight: '16px' }}>
+                  {activeRow.username}
+                </Typography>
+              </Stack>
+            </Stack>
+
+            <Stack
+              sx={{
+                flexDirection: 'row',
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{ flex: 1, mr: 1 }}
+                onClick={() => {
+                  setOpenDeleteModal(false);
+                }}
+              >
+                Batal
+              </Button>
+              <Button
+                variant="contained"
+                sx={{
+                  flex: 1,
+                  backgroundColor: 'warning.main',
+                  '&:hover': {
+                    backgroundColor: 'warning.dark',
+                  },
+                }}
+                onClick={() => {
+                  setOpenDeleteModal(false);
+                  deleteUser(activeRow);
+                }}
+              >
+                Hapus
+              </Button>
             </Stack>
           </Stack>
+        </Modal>
+        {isMobile ? (
+          <style jsx global>{`
+            .MuiDataGrid-root .MuiDataGrid-cell {
+              padding: 0;
+              border-bottom: none !important;
+            }
+            .MuiDataGrid-root .MuiDataGrid-cell:focus-within {
+              outline: none !important;
+            }
+            .MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within {
+              outline: none !important;
+            }
+            .MuiDataGrid-root {
+              border-radius: 0;
+            }
+            .MuiDataGrid-columnHeaders {
+              display: none;
+            }
+            .MuiDataGrid-virtualScroller {
+              padding: 8px 0px;
+            }
+            .MuiToolbar-root {
+              width: 100%;
+            }
+          `}</style>
+        ) : (
+          <style jsx global>{`
+            .MuiDataGrid-row.Mui-even {
+              background-color: #f9fafb;
+            }
+            .MuiDataGrid-row.Mui-odd {
+              background-color: #f2f4f7;
+            }
+            .MuiDataGrid-root .MuiDataGrid-cell {
+              padding: 0;
+              padding-left: 10px;
+            }
+            .MuiDataGrid-root .MuiDataGrid-cell:focus-within {
+              outline: none !important;
+            }
+            .MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within {
+              outline: none !important;
+            }
+            .MuiDataGrid-root {
+              border-radius: 0;
+            }
+          `}</style>
+        )}
+        <DataGrid
+          rows={rows}
+          getRowHeight={() => 'auto'}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 20 },
+            },
+          }}
+          pageSizeOptions={[10, 20, 50]}
+          getRowClassName={(params) =>
+            params.indexRelativeToCurrentPage % 2 === 0 ? 'Mui-even' : 'Mui-odd'
+          }
+          disableRowSelectionOnClick
+          disableColumnMenu
+          columnVisibilityModel={
+            !isMobile
+              ? {
+                  card: false,
+                }
+              : {
+                  profile_image_uri: false,
+                  name: false,
+                  username: false,
+                  type: false,
+                  permissions: false,
+                  action: false,
+                }
+          }
+        />
+      </div>
+    );
+  }, [activeRow, deleteUser, isMobile, openDeleteModal, rows]);
 
-          <Stack
-            sx={{
-              flexDirection: 'row',
-            }}
-          >
-            <Button
-              variant='outlined'
-              sx={{ flex: 1, mr: 1 }}
-              onClick={() => {
-                setOpenDeleteModal(false);
-              }}
-            >
-              Batal
-            </Button>
-            <Button
-              variant='contained'
-              sx={{
-                flex: 1,
-                backgroundColor: 'warning.main',
-                '&:hover': {
-                  backgroundColor: 'warning.dark',
-                },
-              }}
-              onClick={() => {
-                setOpenDeleteModal(false);
-                deleteUser(activeRow);
-              }}
-            >
-              Hapus
-            </Button>
-          </Stack>
-        </Stack>
-      </Modal>
-      {isMobile ? (
-        <style jsx global>{`
-          .MuiDataGrid-root .MuiDataGrid-cell {
-            padding: 0;
-            border-bottom: none !important;
-          }
-          .MuiDataGrid-root .MuiDataGrid-cell:focus-within {
-            outline: none !important;
-          }
-          .MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within {
-            outline: none !important;
-          }
-          .MuiDataGrid-root {
-            border-radius: 0;
-          }
-          .MuiDataGrid-columnHeaders {
-            display: none;
-          }
-          .MuiDataGrid-virtualScroller {
-            padding: 8px 0px;
-          }
-          .MuiToolbar-root {
-            width: 100%;
-          }
-        `}</style>
-      ) : (
-        <style jsx global>{`
-          .MuiDataGrid-row.Mui-even {
-            background-color: #f9fafb;
-          }
-          .MuiDataGrid-row.Mui-odd {
-            background-color: #f2f4f7;
-          }
-          .MuiDataGrid-root .MuiDataGrid-cell {
-            padding: 0;
-            padding-left: 10px;
-          }
-          .MuiDataGrid-root .MuiDataGrid-cell:focus-within {
-            outline: none !important;
-          }
-          .MuiDataGrid-root .MuiDataGrid-columnHeader:focus-within {
-            outline: none !important;
-          }
-          .MuiDataGrid-root {
-            border-radius: 0;
-          }
-        `}</style>
-      )}
-      <DataGrid
-        rows={rows}
-        getRowHeight={() => 'auto'}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 20 },
-          },
-        }}
-        pageSizeOptions={[10, 20, 50]}
-        getRowClassName={(params) =>
-          params.indexRelativeToCurrentPage % 2 === 0 ? 'Mui-even' : 'Mui-odd'
-        }
-        disableRowSelectionOnClick
-        disableColumnMenu
-        columnVisibilityModel={
-          !isMobile
-            ? {
-                card: false,
-              }
-            : {
-                profile_image_uri: false,
-                name: false,
-                username: false,
-                type: false,
-                permissions: false,
-                action: false,
-              }
-        }
-      />
-    </div>
-  );
+  return <CachedReturn />;
 }
