e98b4193363b1e3d2d20ff8edfed264bbd82d505
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-10-26 13:30:46 +0700

    Ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -5,14 +5,17 @@ const nextConfig = {
   },
   images: {
     domains: [
-      "",
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "api-staging.sisva.id",
-      "lottie.host",
+      '',
+      'source.unsplash.com',
+      'images.unsplash.com',
+      'api-staging.sisva.id',
+      'lottie.host',
     ],
   },
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
