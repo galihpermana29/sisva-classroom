40526ac9ed7c000c14d1729503d7383512ddf3cc
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-06 16:12:08 +0700

    Create edit anggota form

--- src/app/administration/[slug]/dashboard/academic/extracurricular/components/FormEditMember.tsx
+++ src/app/administration/[slug]/dashboard/academic/extracurricular/components/FormEditMember.tsx
@@ -3,7 +3,7 @@ import FormLabelSisva from '@/components/FormLabelSisva';
 import { useExtracurricularMembers } from '@/hooks/useExtracurricularMembers';
 import { useExtracurriculars } from '@/hooks/useExtracurriculars';
 import { useStudents } from '@/hooks/useStudents';
-import { TextField } from '@mui/material';
+import { MenuItem, TextField } from '@mui/material';
 
 export default function FormEditMember({
   selectedExtraCurricularId,
@@ -12,16 +12,46 @@ export default function FormEditMember({
   selectedExtraCurricularId: string;
   selectedStudentId: string;
 }) {
-  const { data: extracurricular } = useExtracurriculars();
-  const { data: extracurricularMembers } = useExtracurricularMembers();
-  const { data: students } = useStudents();
+  const { data: extracurriculars, isLoading: isLoading1 } =
+    useExtracurriculars();
+  const { data: extracurricularMembers, isLoading: isLoading2 } =
+    useExtracurricularMembers();
+  const { data: students, isLoading: isLoading3 } = useStudents();
+
+  // skeleton
+  if (isLoading1 || isLoading2 || isLoading3) {
+    return (
+      <FormControlSisva>
+        <FormLabelSisva>Ekstrakurikuler</FormLabelSisva>
+        <TextField disabled />
+        <FormLabelSisva>Anggota</FormLabelSisva>
+        <TextField disabled></TextField>
+      </FormControlSisva>
+    );
+  }
 
   return (
     <FormControlSisva>
       <FormLabelSisva>Ekstrakurikuler</FormLabelSisva>
-      <TextField />
+      <TextField select value={selectedExtraCurricularId}>
+        {extracurriculars.map((extracurricular) => {
+          return (
+            <MenuItem key={extracurricular.id} value={extracurricular.id}>
+              {extracurricular.name}
+            </MenuItem>
+          );
+        })}
+      </TextField>
       <FormLabelSisva>Anggota</FormLabelSisva>
-      <TextField />
+      <TextField select value={selectedStudentId} disabled>
+        {students.map((student) => {
+          return (
+            <MenuItem key={student.id} value={student.id}>
+              {student.name}
+            </MenuItem>
+          );
+        })}
+      </TextField>
     </FormControlSisva>
   );
 }
