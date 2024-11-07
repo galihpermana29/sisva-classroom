3376cecb84e213c5cf368e0c00feb1d87e823cb7
rmvs <nofalrmz@gmail.com>
2024-11-07 19:36:49 +0700

    fix(StudentTaskSection): wrap CardTask in Link for navigation

--- src/app/classroom/(main)/student/container/Section/StudentTaskSection.jsx
+++ src/app/classroom/(main)/student/container/Section/StudentTaskSection.jsx
@@ -6,26 +6,32 @@ import { useGetStudentTask } from "@/app/classroom/(main)/student/usecase/useGet
 import CardTaskSkeleton from "@/app/classroom/shared/presentation/Skeletons/CardTaskSkeleton";
 import EmptyState from "@/app/classroom/shared/presentation/EmptyState/EmptyState";
 import { convertDateTime12To24 } from "@/app/classroom/(main)/student/usecase/convertDateTime12To24";
+import Link from "next/link";
 
 const StudentTaskSection = () => {
   const { tasks, isLoading } = useGetStudentTask();
+  
   return (
     <SectionLayout title={"Tugas yang Akan Datang"}>
       <div className="lg:h-full lg:max-h-[228px] p-1 overflow-auto">
-        <div className="flex flex-row lg:flex-col gap-3">
+        <div className="flex flex-row gap-3 lg:flex-col">
           {isLoading ? (
             [...new Array(3)].map((_, index) => (
               <CardTaskSkeleton key={index} />
             ))
           ) : tasks.length > 0 ? (
             tasks.map((task, index) => (
-              <CardTask
+              <Link
+                href={`/classroom/teacher/class/${task.class_id}/task/${task.id}`}
                 key={index}
+              >
+              <CardTask
                 taskName={task.name}
                 teacherName={task.teacher_name}
                 lessonName={task.subject_name}
                 deadline={convertDateTime12To24(task.deadline)}
               />
+              </Link>
             ))
           ) : (
             <div className="mx-auto">
