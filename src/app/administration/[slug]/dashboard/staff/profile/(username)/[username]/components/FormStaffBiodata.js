1807b0dd972786413f25046dee9423617534777e
galihpermana29 <galihcbn123@gmail.com>
2024-11-12 00:22:51 +0700

    t

--- src/app/administration/[slug]/dashboard/staff/profile/(username)/[username]/components/FormStaffBiodata.js
+++ src/app/administration/[slug]/dashboard/staff/profile/(username)/[username]/components/FormStaffBiodata.js
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Box,
@@ -8,14 +8,14 @@ import {
   Stack,
   TextField,
   Typography,
-} from '@mui/material';
-import Image from 'next/image';
+} from "@mui/material";
+import Image from "next/image";
 
-import { formStaffBiodataFields } from '@/globalcomponents/FormFields';
-import { genders, nationalities, religions } from '@/globalcomponents/Variable';
-import { Cancel } from '@mui/icons-material';
+import { formStaffBiodataFields } from "@/globalcomponents/FormFields";
+import { genders, nationalities, religions } from "@/globalcomponents/Variable";
+import { Cancel } from "@mui/icons-material";
 
-import MediaIcon from '@/assets/icon-Media.svg';
+// import MediaIcon from '@/assets/icon-Media.svg';
 
 export const FormStaffBiodata = ({
   formik,
@@ -53,73 +53,73 @@ export const FormStaffBiodata = ({
     return (
       <>
         {formStaffBiodataFields.map((field) =>
-          field.type === 'text' ? (
+          field.type === "text" ? (
             <Grid
-              sx={{ marginBottom: '8px' }}
+              sx={{ marginBottom: "8px" }}
               item
               xs={12}
               md={field.md}
               key={field.name}
             >
               <Grid item xs={12} md={12}>
-                <Typography variant='body2' fontWeight={500} fontSize={14}>
+                <Typography variant="body2" fontWeight={500} fontSize={14}>
                   {field.label}
                 </Typography>
               </Grid>
               <Grid item xs={12} md={12}>
-                <Typography variant='body2' fontWeight={400} fontSize={14}>
-                  {formik.values[field.name] ? formik.values[field.name] : ''}
+                <Typography variant="body2" fontWeight={400} fontSize={14}>
+                  {formik.values[field.name] ? formik.values[field.name] : ""}
                 </Typography>
               </Grid>
             </Grid>
-          ) : field.type === 'select' ? (
+          ) : field.type === "select" ? (
             <Grid
-              sx={{ marginBottom: '8px' }}
+              sx={{ marginBottom: "8px" }}
               item
               xs={12}
               md={field.md}
               key={field.name}
             >
               <Grid item xs={12} md={12}>
-                <Typography variant='body2' fontWeight={500} fontSize={14}>
+                <Typography variant="body2" fontWeight={500} fontSize={14}>
                   {field.label}
                 </Typography>
               </Grid>
               <Grid item xs={12} md={12}>
-                <Typography variant='body2' fontWeight={400} fontSize={14}>
-                  {field.name === 'gender' ? (
+                <Typography variant="body2" fontWeight={400} fontSize={14}>
+                  {field.name === "gender" ? (
                     <RenderGender value={formik.values[field.name]} />
-                  ) : field.name === 'nationality' ? (
+                  ) : field.name === "nationality" ? (
                     <RenderNationality value={formik.values[field.name]} />
-                  ) : field.name === 'religion' ? (
+                  ) : field.name === "religion" ? (
                     <RenderReligion value={formik.values[field.name]} />
                   ) : null}
-                  {formik.values[field.name] ? '' : '-'}
+                  {formik.values[field.name] ? "" : "-"}
                 </Typography>
               </Grid>
             </Grid>
-          ) : field.type === 'photo' ? (
+          ) : field.type === "photo" ? (
             <Grid xs={12} item key={field.name}>
-              <Typography variant='body2' fontWeight={500} fontSize={14}>
+              <Typography variant="body2" fontWeight={500} fontSize={14}>
                 {field.label}
               </Typography>
               <Box
                 sx={{
                   mt: 1,
                   p: 1,
-                  width: 'fit-content',
-                  backgroundColor: 'base.base20',
+                  width: "fit-content",
+                  backgroundColor: "base.base20",
                   borderRadius: 2,
                 }}
               >
-                <Box sx={{ height: 96, width: 96, position: 'relative' }}>
+                <Box sx={{ height: 96, width: 96, position: "relative" }}>
                   <Image
-                    alt='Image'
+                    alt="Image"
                     src={`https://api-staging.sisva.id/file/v1/files/${
                       formik.values[field.name]
                     }?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
-                    layout='fill'
-                    objectFit='cover'
+                    layout="fill"
+                    objectFit="cover"
                   />
                 </Box>
               </Box>
@@ -132,9 +132,9 @@ export const FormStaffBiodata = ({
     return (
       <>
         {formStaffBiodataFields.map((field) =>
-          field.type === 'text' ? (
+          field.type === "text" ? (
             <Grid item xs={12} md={field.md} key={field.name}>
-              <Typography variant='body2' fontWeight={600} mb={1}>
+              <Typography variant="body2" fontWeight={600} mb={1}>
                 {field.label}
               </Typography>
               <TextField
@@ -147,9 +147,9 @@ export const FormStaffBiodata = ({
                 }
               />
             </Grid>
-          ) : field.type === 'select' ? (
+          ) : field.type === "select" ? (
             <Grid item xs={12} md={field.md} key={field.name}>
-              <Typography variant='body2' fontWeight={600} mb={1}>
+              <Typography variant="body2" fontWeight={600} mb={1}>
                 {field.label}
               </Typography>
 
@@ -164,15 +164,15 @@ export const FormStaffBiodata = ({
                   startAdornment: formik.values[field.name] && (
                     <Cancel
                       onClick={() => {
-                        setTypeFilter('');
+                        setTypeFilter("");
                       }}
                       sx={{
                         fontSize: 14,
-                        color: 'base.base50',
-                        cursor: 'pointer',
-                        transform: 'translateX(-4px)',
-                        '&:hover': {
-                          color: 'base.base60',
+                        color: "base.base50",
+                        cursor: "pointer",
+                        transform: "translateX(-4px)",
+                        "&:hover": {
+                          color: "base.base60",
                         },
                       }}
                     />
@@ -186,9 +186,9 @@ export const FormStaffBiodata = ({
                 ))}
               </TextField>
             </Grid>
-          ) : field.type === 'photo' ? (
+          ) : field.type === "photo" ? (
             <Grid xs={12} item key={field.name}>
-              <Typography variant='body2' fontWeight={500} fontSize={14}>
+              <Typography variant="body2" fontWeight={500} fontSize={14}>
                 {field.label}
               </Typography>
               <Stack
@@ -199,20 +199,20 @@ export const FormStaffBiodata = ({
                 <Box
                   sx={{
                     p: 1,
-                    width: 'fit-content',
-                    backgroundColor: 'base.base20',
+                    width: "fit-content",
+                    backgroundColor: "base.base20",
                     borderRadius: 2,
                   }}
                 >
                   {formik.values[field.name] ? (
-                    <Box sx={{ height: 96, width: 96, position: 'relative' }}>
+                    <Box sx={{ height: 96, width: 96, position: "relative" }}>
                       <Image
-                        alt='Image'
+                        alt="Image"
                         src={`https://api-staging.sisva.id/file/v1/files/${
                           formik.values[field.name]
                         }?school_id=0a49a174-9ff5-464d-86c2-3eb1cd0b284e`}
-                        layout='fill'
-                        objectFit='cover'
+                        layout="fill"
+                        objectFit="cover"
                       />
                     </Box>
                   ) : (
@@ -222,39 +222,32 @@ export const FormStaffBiodata = ({
                         width: 96,
                         flex: 1,
                       }}
-                    >
-                      <Image
-                        alt='Image'
-                        src={MediaIcon}
-                        // layout='fill'
-                        // objectFit='cover'
-                      />
-                    </Box>
+                    ></Box>
                   )}
                 </Box>
-                <label htmlFor='image-input'>
+                <label htmlFor="image-input">
                   <Button
                     fullWidth
-                    variant='outlined'
-                    size='small'
-                    sx={{ m: '8px 0 4px' }}
+                    variant="outlined"
+                    size="small"
+                    sx={{ m: "8px 0 4px" }}
                   >
                     Ubah Foto
                     <input
-                      name={'profile_image_uri'}
-                      accept='image/*'
-                      id='image-input'
-                      type='file'
+                      name={"profile_image_uri"}
+                      accept="image/*"
+                      id="image-input"
+                      type="file"
                       style={{
-                        position: 'absolute',
-                        opacity: '0',
-                        border: '1px solid red',
+                        position: "absolute",
+                        opacity: "0",
+                        border: "1px solid red",
                       }}
                       onChange={handleImageChange}
                     />
                   </Button>
                 </label>
-                <Button fullWidth variant='outlined' size='small'>
+                <Button fullWidth variant="outlined" size="small">
                   Hapus
                 </Button>
               </Stack>
