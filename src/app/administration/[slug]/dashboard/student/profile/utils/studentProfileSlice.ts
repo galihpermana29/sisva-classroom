9bccba08a8a4a5a97036c87b1596132e12700024
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-20 15:15:53 +0700

    Create student slice

---
+++ src/app/administration/[slug]/dashboard/student/profile/utils/studentProfileSlice.ts
@@ -0,0 +1,46 @@
+import { RootState } from "@/app/administration/store";
+import type { Permission, Role, SortDirection } from "@/globalcomponents/types";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+export type SortField = "name" | "username" | "";
+
+interface StudentProfileState {
+  searchText: string;
+  sortField: SortField;
+  sortDirection: SortDirection;
+}
+
+const initialState: StudentProfileState = {
+  searchText: "",
+  sortField: "",
+  sortDirection: "",
+};
+
+export const studentProfileSlice = createSlice({
+  name: "student/profile",
+  initialState,
+  reducers: {
+    setSearchText: (state, action: PayloadAction<string>) => {
+      state.searchText = action.payload;
+    },
+    setSortField: (state, action: PayloadAction<SortField>) => {
+      state.sortField = action.payload;
+    },
+    setSortDirection: (state, action: PayloadAction<SortDirection>) => {
+      state.sortDirection = action.payload;
+    },
+  },
+});
+
+export const { setSearchText, setSortField, setSortDirection } =
+  studentProfileSlice.actions;
+
+export const selectSearchText = (state: RootState) =>
+  state.studentProfile.searchText;
+export const selectSortField = (state: RootState) =>
+  state.studentProfile.sortField;
+export const selectSortDirection = (state: RootState) =>
+  state.studentProfile.sortDirection;
+
+export const studentProfileReducer = studentProfileSlice.reducer;
--- src/app/administration/store.ts
+++ src/app/administration/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import staffAttendanceReducer from "./[slug]/dashboard/staff/attendance/utils/staffAttendanceSlice";
 import { staffProfileReducer } from "./[slug]/dashboard/staff/profile/utils/staffProfileSlice";
 import studentAttendanceReducer from "./[slug]/dashboard/student/attendance/utils/studentAttendanceSlice";
+import { studentProfileReducer } from "./[slug]/dashboard/student/profile/utils/studentProfileSlice";
 
 export const store = configureStore({
   reducer: {
     staffAttendance: staffAttendanceReducer,
     studentAttendance: studentAttendanceReducer,
     staffProfile: staffProfileReducer,
+    studentProfile: studentProfileReducer,
   },
 });
 
