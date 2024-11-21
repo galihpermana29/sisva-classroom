0fca1f0df18990e1d704340a228d3ac17a06ede3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-21 11:15:28 +0700

    Refactor UsersApi

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,55 +1,47 @@
-import axios from "axios";
-
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import { getBearerToken, getSchoolId, getUserId } from ".";
 
-const BEARER_TOKEN = getBearerToken();
-const USER_ID = getUserId();
-const SCHOOL_ID = getSchoolId();
+const UsersAPI = new (class {
+  private api: AxiosInstance;
+  constructor() {
+    this.api = axios.create({
+      baseURL: "https://api-staging.sisva.id/user/v1",
+    });
+  }
 
-const api = axios.create({
-  baseURL: "https://api-staging.sisva.id/user/v1",
-});
-
-const UsersAPI = {
-  createUser(payload) {
-    const headers = {
+  private getRequestConfig(
+    additionalHeaders: Record<string, string> = {}
+  ): AxiosRequestConfig {
+    const defaultHeaders = {
       "X-Sisva-Source": "tenant.user.test",
-      "X-Sisva-UserID": USER_ID,
-      "X-Sisva-SchoolID": SCHOOL_ID,
-      Authorization: `Bearer ${BEARER_TOKEN}`,
-    };
-    return api.post(`/users`, payload, { headers });
-  },
-
-  getAllUsers(params = "staff,teacher,student") {
-    const headers = {
-      "X-Sisva-Source": "test",
-      "X-Sisva-UserID": USER_ID,
-      "X-Sisva-SchoolID": SCHOOL_ID,
-      Authorization: `Bearer ${BEARER_TOKEN}`,
-    };
-    return api.get(`/users?types=${params}`, { headers });
-  },
-
-  getUserById(id) {
-    const headers = {
-      "X-Sisva-Source": "test",
-      "X-Sisva-UserID": USER_ID,
-      "X-Sisva-SchoolID": SCHOOL_ID,
+      "X-Sisva-UserID": getUserId(),
+      "X-Sisva-SchoolID": getSchoolId(),
       Authorization: `Bearer ${getBearerToken()}`,
     };
-    return api.get(`/users/${id}`, { headers });
-  },
 
-  updateUserById(payload, id) {
-    const headers = {
-      "X-Sisva-Source": "tenant.user.test",
-      "X-Sisva-UserID": USER_ID,
-      "X-Sisva-SchoolID": SCHOOL_ID,
-      Authorization: `Bearer ${BEARER_TOKEN}`,
+    return {
+      headers: {
+        ...defaultHeaders,
+        ...additionalHeaders,
+      },
     };
-    return api.patch(`/users/${id}`, payload, { headers });
-  },
-};
+  }
+
+  async createUser(payload: any) {
+    return this.api.post("/users", payload, this.getRequestConfig());
+  }
+
+  async getAllUsers(params: string = "staff,teacher,student") {
+    return this.api.get(`/users?types=${params}`, this.getRequestConfig());
+  }
+
+  async getUserById(id: string | number) {
+    return this.api.get(`/users/${id}`, this.getRequestConfig());
+  }
+
+  async updateUserById(payload: any, id: string) {
+    return this.api.patch(`/users/${id}`, payload, this.getRequestConfig());
+  }
+})();
 
 export default UsersAPI;
