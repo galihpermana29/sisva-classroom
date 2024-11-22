7b5d524e56400242f840765d181258837d9a7fc2
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-22 14:45:13 +0700

    Debounce study program input

--- src/app/administration/[slug]/dashboard/academic/study-program/components/FormAddStudyProgram.tsx
+++ src/app/administration/[slug]/dashboard/academic/study-program/components/FormAddStudyProgram.tsx
@@ -1,98 +1,30 @@
 "use client";
 
-import {
-  IconButton,
-  InputAdornment,
-  MenuItem,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-
 import { formAddStudyProgramFields } from "@/globalcomponents/FormFields";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useState } from "react";
+import { useDebouncedCallback } from "@mantine/hooks";
+import { Stack, TextField, Typography } from "@mui/material";
 
 export const FormAddStudyProgram = ({ formik }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
+  const setFieldValue = useDebouncedCallback((fieldName, value) => {
+    formik.setFieldValue(fieldName, value);
+  }, 50);
 
   return (
     <>
-      {formAddStudyProgramFields.map((field) =>
-        field.type === "text" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600} mb={0.5}>
-              {field.label}
-            </Typography>
-            <TextField
-              name={field.name}
-              placeholder={field.placeholder}
-              fullWidth
-              value={formik.values[field.name]}
-              onChange={(e) => formik.setFieldValue(field.name, e.target.value)}
-            />
-          </Stack>
-        ) : field.type === "password" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600} mb={0.5}>
-              {field.label}
-            </Typography>
-            <TextField
-              type={showPassword ? "text" : "password"}
-              name={field.name}
-              placeholder={field.placeholder}
-              fullWidth
-              value={formik.values[field.name]}
-              onChange={(e) => formik.setFieldValue(field.name, e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() =>
-                        field.name === "password"
-                          ? setShowPassword(!showPassword)
-                          : setShowPasswordConfirm(!showPasswordConfirm)
-                      }
-                    >
-                      {field.name === "password" &&
-                        (showPassword ? (
-                          <VisibilityOff sx={{ fontSize: 16 }} />
-                        ) : (
-                          <Visibility sx={{ fontSize: 16 }} />
-                        ))}
-                      {field.name === "password_confirm" &&
-                        (showPasswordConfirm ? (
-                          <VisibilityOff sx={{ fontSize: 16 }} />
-                        ) : (
-                          <Visibility sx={{ fontSize: 16 }} />
-                        ))}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Stack>
-        ) : field.type === "select" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600} mb={0.5}>
-              {field.label}
-            </Typography>
-            <TextField
-              select
-              value={formik.values[field.name]}
-              onChange={(e) => formik.setFieldValue(field.name, e.target.value)}
-              sx={{ flex: { xs: 1, lg: 0 } }}
-            >
-              {field.data.map((option) => (
-                <MenuItem key={option.slug} value={option.slug}>
-                  <Typography fontSize={14}>{option.title}</Typography>
-                </MenuItem>
-              ))}
-            </TextField>
-          </Stack>
-        ) : null
-      )}
+      {formAddStudyProgramFields.map((field) => (
+        <Stack sx={{ my: 1 }} key={field.name}>
+          <Typography variant="body2" fontWeight={600} mb={0.5}>
+            {field.label}
+          </Typography>
+          <TextField
+            name={field.name}
+            placeholder={field.placeholder}
+            fullWidth
+            defaultValue={formik.values[field.name]}
+            onChange={(e) => setFieldValue(field.name, e.target.value)}
+          />
+        </Stack>
+      ))}
     </>
   );
 };
