a91e4e754097a629523ed9f17a216ca5da272b66
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-22 16:20:40 +0700

    Refactor Add subject form

--- src/app/administration/[slug]/dashboard/academic/curriculum/components/FormAddSubject.tsx
+++ src/app/administration/[slug]/dashboard/academic/curriculum/components/FormAddSubject.tsx
@@ -1,37 +1,15 @@
 "use client";
 
-import {
-  Box,
-  Chip,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { MenuItem, Stack, TextField, Typography } from "@mui/material";
+import { useMemo } from "react";
 
-import { formAddSubjectFields } from "@/globalcomponents/FormFields";
-import { permissions } from "@/globalcomponents/Variable";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useState } from "react";
-
+// TODO: this from is very lag, please rework
 export const FormAddSubject = ({ formik, tableData, studyProgram }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
-
-  const updatedSubjectFields = formAddSubjectFields.map((field) => {
-    if (field.name == "curriculum_name") {
-      field.data = tableData.map((td) => {
-        return { slug: td.id, title: td.name };
-      });
-    }
-
-    return field;
-  });
+  const curriculumOptions = useMemo(() => {
+    return tableData.map((td) => {
+      return { slug: td.id, title: td.name };
+    });
+  }, [tableData]);
 
   const studyProgramData = studyProgram.map((dt) => {
     return { slug: dt.id, title: dt.name, code: dt.code };
@@ -39,185 +17,82 @@ export const FormAddSubject = ({ formik, tableData, studyProgram }) => {
 
   return (
     <>
-      {updatedSubjectFields.map((field) =>
-        field.type === "text" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600} mb={0.5}>
-              {field.label}
-            </Typography>
-            <TextField
-              name={field.name}
-              placeholder={field.placeholder}
-              fullWidth
-              value={formik.values[field.name]}
-              onChange={(e) => formik.setFieldValue(field.name, e.target.value)}
-            />
-          </Stack>
-        ) : field.type === "password" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600} mb={0.5}>
-              {field.label}
-            </Typography>
-            <TextField
-              type={showPassword ? "text" : "password"}
-              name={field.name}
-              placeholder={field.placeholder}
-              fullWidth
-              value={formik.values[field.name]}
-              onChange={(e) => formik.setFieldValue(field.name, e.target.value)}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={() =>
-                        field.name === "password"
-                          ? setShowPassword(!showPassword)
-                          : setShowPasswordConfirm(!showPasswordConfirm)
-                      }
-                    >
-                      {field.name === "password" &&
-                        (showPassword ? (
-                          <VisibilityOff sx={{ fontSize: 16 }} />
-                        ) : (
-                          <Visibility sx={{ fontSize: 16 }} />
-                        ))}
-                      {field.name === "password_confirm" &&
-                        (showPasswordConfirm ? (
-                          <VisibilityOff sx={{ fontSize: 16 }} />
-                        ) : (
-                          <Visibility sx={{ fontSize: 16 }} />
-                        ))}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Stack>
-        ) : field.type === "select" ? (
-          field.name === "curriculum_name" ? (
-            <Stack sx={{ my: 1 }} key={field.name}>
-              <Typography variant="body2" fontWeight={600} mb={0.5}>
-                {field.label}
-              </Typography>
-              <TextField
-                select
-                value={formik.values[field.name]}
-                onChange={(e) => {
-                  formik.setFieldValue(field.name, e.target.value);
-                }}
-                sx={{ flex: { xs: 1, lg: 0 } }}
-              >
-                {field.data.map((option) => (
-                  <MenuItem key={option.slug} value={option.slug}>
-                    <Typography fontSize={14}>{option.title}</Typography>
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Stack>
-          ) : field.name === "study_program" ? (
-            <Stack sx={{ my: 1 }} key={field.name}>
-              <Typography variant="body2" fontWeight={600} mb={0.5}>
-                {field.label}
-              </Typography>
-              <TextField
-                select
-                value={formik.values[field.name]}
-                onChange={(e) =>
-                  formik.setFieldValue(field.name, e.target.value)
-                }
-                sx={{ flex: { xs: 1, lg: 0 } }}
-              >
-                {studyProgramData?.length
-                  ? studyProgramData?.map((option) => (
-                      <MenuItem key={option.slug} value={option.slug}>
-                        <Typography fontSize={14}>{option.title}</Typography>
-                      </MenuItem>
-                    ))
-                  : ["Program Studi Tidak Tersedia"].map((option, idx) => (
-                      <MenuItem
-                        selected={idx == 0}
-                        disabled
-                        key={idx}
-                        value={option}
-                      >
-                        <Typography fontSize={14}>{option}</Typography>
-                      </MenuItem>
-                    ))}
-              </TextField>
-            </Stack>
-          ) : (
-            <Stack sx={{ my: 1 }} key={field.name}>
-              <Typography variant="body2" fontWeight={600} mb={0.5}>
-                {field.label}
-              </Typography>
-              <TextField
-                select
-                value={formik.values[field.name]}
-                onChange={(e) =>
-                  formik.setFieldValue(field.name, e.target.value)
-                }
-                sx={{ flex: { xs: 1, lg: 0 } }}
-              >
-                {field.data.map((option) => (
-                  <MenuItem key={option.slug} value={option.slug}>
-                    <Typography fontSize={14}>{option.title}</Typography>
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Stack>
-          )
-        ) : field.type === "multiple-select" ? (
-          <Stack sx={{ my: 1 }} key={field.name}>
-            <Typography variant="body2" fontWeight={600}>
-              {field.label}
-            </Typography>
-
-            <FormControl sx={{ width: "100%" }}>
-              {/* <InputLabel id="demo-multiple-chip-label">Chip</InputLabel> */}
-              <Select
-                multiple
-                value={formik.values[field.name]}
-                onChange={(e) =>
-                  formik.setFieldValue(field.name, e.target.value)
-                }
-                input={<OutlinedInput sx={{ p: 0 }} />}
-                renderValue={(selected) => (
-                  <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                    {selected.map((permission) => {
-                      let tempPermission;
-                      permissions.map((item) => {
-                        if (item.slug === permission) {
-                          tempPermission = item.title;
-                        }
-                      });
-                      return (
-                        <Chip
-                          key={permission}
-                          label={tempPermission}
-                          color="primary"
-                        />
-                      );
-                    })}
-                  </Box>
-                )}
-                MenuProps={{
-                  anchorOrigin: { vertical: "top", horizontal: "center" },
-                  transformOrigin: {
-                    vertical: "bottom",
-                    horizontal: "center",
-                  },
-                }}
-              >
-                {field.data.map((option) => (
-                  <MenuItem key={option.slug} value={option.slug}>
-                    <Typography fontSize={14}>{option.title}</Typography>
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Stack>
-        ) : null
-      )}
+      <Stack sx={{ my: 1 }}>
+        <Typography variant="body2" fontWeight={600} mb={0.5}>
+          {"Nama Kurikulum"}
+        </Typography>
+        <TextField
+          select
+          value={formik.values["curriculum_name"]}
+          onChange={(e) => {
+            formik.setFieldValue("curriculum_name", e.target.value);
+          }}
+          sx={{ flex: { xs: 1, lg: 0 } }}
+        >
+          {curriculumOptions.map((option) => (
+            <MenuItem key={option.slug} value={option.slug}>
+              <Typography fontSize={14}>{option.title}</Typography>
+            </MenuItem>
+          ))}
+        </TextField>
+      </Stack>
+      <Stack sx={{ my: 1 }}>
+        <Typography variant="body2" fontWeight={600} mb={0.5}>
+          Program Studi
+        </Typography>
+        <TextField
+          select
+          value={formik.values["study_program"]}
+          onChange={(e) =>
+            formik.setFieldValue("study_program", e.target.value)
+          }
+          sx={{ flex: { xs: 1, lg: 0 } }}
+        >
+          {studyProgramData?.length
+            ? studyProgramData?.map((option) => (
+                <MenuItem key={option.slug} value={option.slug}>
+                  <Typography fontSize={14}>{option.title}</Typography>
+                </MenuItem>
+              ))
+            : ["Program Studi Tidak Tersedia"].map((option, idx) => (
+                <MenuItem selected={idx == 0} disabled key={idx} value={option}>
+                  <Typography fontSize={14}>{option}</Typography>
+                </MenuItem>
+              ))}
+        </TextField>
+      </Stack>
+      <Stack sx={{ my: 1 }}>
+        <Typography variant="body2" fontWeight={600} mb={0.5}>
+          Mata Pelajaran
+        </Typography>
+        <TextField
+          name="subject"
+          placeholder="Mata Pelajaran"
+          fullWidth
+          value={formik.values["subject"]}
+          onChange={(e) => formik.setFieldValue("subject", e.target.value)}
+        />
+      </Stack>
+      <Stack sx={{ my: 1 }}>
+        <Typography variant="body2" fontWeight={600} mb={0.5}>
+          Tipe
+        </Typography>
+        <TextField
+          select
+          value={formik.values["subject_type"]}
+          onChange={(e) => formik.setFieldValue("subject_type", e.target.value)}
+          sx={{ flex: { xs: 1, lg: 0 } }}
+        >
+          {[
+            { slug: "mandatory", title: "Wajib" },
+            { slug: "optional", title: "Pilihan" },
+          ].map((option) => (
+            <MenuItem key={option.slug} value={option.slug}>
+              <Typography fontSize={14}>{option.title}</Typography>
+            </MenuItem>
+          ))}
+        </TextField>
+      </Stack>
     </>
   );
 };
