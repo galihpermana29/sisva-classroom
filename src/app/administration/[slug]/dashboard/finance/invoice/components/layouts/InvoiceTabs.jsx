e1a0e4227402d46666daf62ca11fb4de7e578619
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-11-25 16:07:03 +0700

    Fix kategori not reset between tab switch

--- src/app/administration/[slug]/dashboard/finance/invoice/components/layouts/InvoiceTabs.jsx
+++ src/app/administration/[slug]/dashboard/finance/invoice/components/layouts/InvoiceTabs.jsx
@@ -8,6 +8,7 @@ import { TAB_FIELD_NAME, TabsSelector } from "@/components/TabSelector";
 import { CURRENT_PAGE_NAME, INVOICE_TABS } from "../../constants";
 import { useActiveInvoiceTab } from "../../hooks/useActiveInvoiceTab";
 import { SORT_PARAM_NAME } from "../../hooks/useSortKey";
+import { KATEGORI_FIELD_NAME } from "../filters/KategoriSelect";
 import { STATUS_FIELD_NAME } from "../filters/StatusSelect";
 
 /** @description Component to display tab selector */
@@ -25,6 +26,7 @@ export const InvoiceTabs = () => {
       params.delete(SORT_PARAM_NAME);
       params.delete(CURRENT_PAGE_NAME);
       params.delete(STATUS_FIELD_NAME);
+      params.delete(KATEGORI_FIELD_NAME);
 
       return params.toString();
     },
